295a7ee224c05633c92cb66de9c538a5f251d19d
NubPaws <dxroomram@gmail.com>
2025-03-22 17:13:46 +0200

    Created booking module

--- src/booking/bookings.module.ts
+++ src/booking/bookings.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { Booking } from './entities/booking.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Booking } from './booking.entity';
+import { ShowtimesModule } from '../showtimes/showtimes.module';
+import { BookingsController } from './bookings.controller';
+import { BookingsService } from './bookings.service';
+import { BookingsRepository } from './bookings.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Booking])],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([Booking]), ShowtimesModule],
+  controllers: [BookingsController],
+  providers: [BookingsService, BookingsRepository],
 })
 export class BookingsModule {}
